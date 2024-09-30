de90bd473a2f2039ff1906fc585b7969a9a1754b
Rasmus Räihä <68077022+Ranzz02@users.noreply.github.com>
2024-09-30 16:33:25 +0300

    working on chats

--- src/ws/chat/hub.go
+++ src/ws/chat/hub.go
@@ -40,26 +40,45 @@ func (h *Hub) run() {
 		case client := <-h.Register:
 			h.mu.Lock()
 			h.Clients[client.UserID] = client
+
+			// Check for room
 			room, exists := h.Rooms[client.RoomID]
-			if exists {
-				room.mu.Lock()
-				room.Clients[client.UserID] = client
-				room.mu.Unlock()
+			if !exists {
+				room = &Room{
+					ID:      client.RoomID,
+					Clients: make(map[string]*Client),
+				}
+				h.Rooms[client.RoomID] = room
 			}
+
+			// Add client to the room
+			room.mu.Lock()
+			room.Clients[client.UserID] = client
+			room.mu.Unlock()
+
 			h.mu.Unlock()
 		case client := <-h.Unregister:
 			h.mu.Lock()
 			if existingClient, ok := h.Clients[client.UserID]; ok {
 				delete(h.Clients, existingClient.UserID)
+
+				// Remove the client from the room if room exists
 				if room, exists := h.Rooms[client.RoomID]; exists {
 					room.mu.Lock()
 					delete(room.Clients, client.UserID)
+
+					if len(room.Clients) <= 0 {
+						delete(h.Rooms, client.RoomID)
+					}
+
 					room.mu.Unlock()
 				}
 			}
 			h.mu.Unlock()
 		case message := <-h.Broadcast:
 			h.mu.Lock()
+
+			// Broadcast message to all clients in room
 			if room, exists := h.Rooms[message.RoomID]; exists {
 				room.mu.Lock()
 				for _, client := range room.Clients {
