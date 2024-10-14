1660311280ffd332bf43c2135268e22084ccb477
Stenoliv <stenbaco@arcada.fi>
2024-10-14 18:05:57 +0300

    Fixing linedataid error
    fix to server error

--- src/db/models/line_data.go
+++ src/db/models/line_data.go
@@ -6,11 +6,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type LinePoint struct {
-	Point      float64  `json:"point"`
-	LineDataID string   `gorm:"size:21; not null;" json:"-"`
-	LineData   LineData `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
-}
 type LineData struct {
 	Base
 	WhiteboardID string         `gorm:"size:21; not null;" json:"-"`
@@ -28,21 +23,9 @@ func (l *LineData) SaveLineData(tx *gorm.DB) error {
 	}
 	return nil
 }
-func (l *LinePoint) SaveLinePoint(tx *gorm.DB) error {
-	if err := tx.Create(l).Error; err != nil {
-		return fmt.Errorf("failed to save line data: %w", err)
-	}
-	return nil
-}
 func (ld *LineData) BeforeCreate(tx *gorm.DB) error {
 	err := ld.GenerateID()
 	if err != nil {
-		return err
-	}
-	return nil
-}
-func (ld *LineData) AfterFind(tx *gorm.DB) error {
-	if err := tx.Model(&LinePoint{}).Where("line_data_id = ?", ld.ID).Order("created_at ASC").Find(&ld).Error; err != nil {
 		return err
 	}
 	return nil
---
+++ src/db/models/line_points.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type LinePoint struct {
+	Point      float64  `json:"point"`
+	LineDataID string   `gorm:"size:21; not null;" json:"-"`
+	LineData   LineData `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
+}
+
+func (l *LinePoint) SaveLinePoint(tx *gorm.DB) error {
+	if err := tx.Create(l).Error; err != nil {
+		return fmt.Errorf("failed to save line data: %w", err)
+	}
+	return nil
+}
+func (ld *LineData) AfterFind(tx *gorm.DB) error {
+	if err := tx.Model(&LinePoint{}).Where("line_data_id = ?", ld.ID).Order("created_at ASC").Find(&ld).Error; err != nil {
+		return err
+	}
+	return nil
+}
